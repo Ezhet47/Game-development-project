ca0617b7e92846acd292db9571f29da717170068
Noah <tang20001220@gmail.com>
2025-10-23 16:59:50 +0200

    Animations_MainScene

---
+++ Reclaimer/Assets/Scripts/AnimatorBoolHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimatorBoolHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private string boolParameter;
+
+    private void Awake()
+    {
+        if (animator) 
+            animator.SetBool(boolParameter, false); 
+    }
+
+    private void OnDisable()
+    {
+        if (animator) 
+            animator.SetBool(boolParameter, false); 
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (animator) 
+            animator.SetBool(boolParameter, true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (animator) 
+            animator.SetBool(boolParameter, false);
+    }
+}
